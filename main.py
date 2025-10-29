df7b0677909ca5dc0655a43ac5f9e75617eeaa22
Leonid_Gulaev <cybernooblo92@mail.ru>
2025-10-29 23:46:12 +0300

    fix: Interaction logic has been simplified

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def main() -> None:
     api = HHVacancyAPI()
     path = os.path.join(os.path.dirname(__file__), "data", "vacancy.json")
     storage = JSONVacancyStorage(path)
+
     region_choice = input("Введите населенный пункт(город, регион и т.п.): ")
     while True:
         print("\n1. Поиск вакансий")
@@ -18,48 +19,33 @@ def main() -> None:
         print("4. Выход из программы")
         choice = input("\nВыберите действие: ")
 
-        if choice == '1':
+        if choice == "1":
             query = input("Введите название вакансии: ")
-            vacancies = api.fetch_vacancies(query, region_choice)
-            for item in vacancies:
-                if item['salary']:
-                    vacancy = Vacancy(item['name'], item['area']['name'], item['alternate_url'],
-                                      item['salary'].get('from'), item['salary'].get('to'),
-                                      item['snippet']['requirement'])
+            for item in api.fetch_vacancies(query, region_choice):
+                vacancy = Vacancy.from_hh_item(item)   # ▼ вся маппинг-логика внутри класса
+                print(vacancy)
+                storage.add_vacancy(vacancy)
+
+        elif choice == "2":
+            top_amount = input("Введите количество топ-вакансий: ")
+            if not top_amount.isdigit():
+                print("Вы ввели не число")
+                continue
+            all_vacancies = storage.get_vacancies("")
+            top_vacancies = sorted(all_vacancies, reverse=True)
+            if len(top_vacancies) >= int(top_amount):
+                for vacancy in top_vacancies[:int(top_amount)]:
                     print(vacancy)
-                    storage.add_vacancy(vacancy)
-                else:
-                    vacancy = Vacancy(item['name'], item['area']['name'], item['alternate_url'], item['salary'], "",
-                                      item['snippet']['requirement'])
-                    print(vacancy)
-                    storage.add_vacancy(vacancy)
+            else:
+                print("Общее количество вакансий меньше количества запроса")
 
-        elif choice == '2':
-            while True:
-                top_amount = input("Введите количество топ-вакансий: ")
-                if not top_amount.isdigit():
-                    print("Вы ввели не число")
-                    continue
-                all_vacancies = storage.get_vacancies("")
-
-                top_vacancies = sorted(all_vacancies, reverse=True)
-                if len(top_vacancies) >= int(top_amount):
-                    for vacancy in top_vacancies[:int(top_amount)]:
-                        print(vacancy)
-                    break
-                else:
-                    print("Общее количество вакансий меньше количества запроса")
-                    continue
-
-        elif choice == '3':
+        elif choice == "3":
             keyword = input("Введите ключевое слово для поиска в описании: ")
-            vacancies = storage.get_vacancies(keyword)
-            for vacancy in vacancies:
+            for vacancy in storage.get_vacancies(keyword):
                 print(vacancy)
 
-        elif choice == '4':
+        elif choice == "4":
             break
-
         else:
             print("Неверный выбор, попробуйте снова.")
 
